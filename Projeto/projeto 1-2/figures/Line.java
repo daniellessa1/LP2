6fa324fa04db640ba014aa82f2b5786689e7700c
daniellessa1 <88343369+daniellessa1@users.noreply.github.com>
2021-11-14 10:48:45 -0300

    Update Line.java

--- Projeto/projeto 1-2/figures/Line.java
+++ Projeto/projeto 1-2/figures/Line.java
@@ -3,24 +3,23 @@ package figures;
 import java.awt.*;
 import java.awt.Color;
 import java.awt.geom.Line2D;
+import java.awt.geom.AffineTransform;
 
 public class Line extends Figure {
-    public Line (int x, int y, int w, int h, Color contorno, Color fundo) {
-        super(x,y, w,h, contorno,fundo);
-    }
-
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+    public Line (int x, int y, int w, int h,int angle, Color contorno, Color fundo) {
+        super(x,y,w,h,angle,contorno,fundo);
     }
 
     public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
+        AffineTransform old = g2d.getTransform();
+        g2d.rotate(Math.toRadians(angle), x + w/2, y + h/2);
         if (focused){
             g2d.setColor(Color.red);
             g2d.drawRect(this.x - 2, this.y - 2, this.w  + 2, this.h  + 2);
         }
         g2d.setColor(this.contorno);
         g2d.drawLine(this.x, this.y, this.w + this.x, this.h + this.y);
+        g2d.setTransform(old);
     }
 }
