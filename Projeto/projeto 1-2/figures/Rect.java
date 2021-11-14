066e54ddfae7afd3edc3aead80b72d33ae95af1e
daniellessa1 <88343369+daniellessa1@users.noreply.github.com>
2021-11-14 10:49:20 -0300

    Update Rect.java

--- Projeto/projeto 1-2/figures/Rect.java
+++ Projeto/projeto 1-2/figures/Rect.java
@@ -2,19 +2,19 @@ package figures;
 
 import java.awt.*;
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
+
+
 
 public class Rect extends Figure {
-    public Rect (int x, int y, int w, int h, Color contorno, Color fundo) {
-        super(x,y, w,h, contorno,fundo);
-    }
-
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+    public Rect (int x, int y, int w, int h,int angle, Color contorno, Color fundo) {
+        super(x,y,w,h,angle,contorno,fundo);
     }
 
     public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
+        AffineTransform old = g2d.getTransform();
+        g2d.rotate(Math.toRadians(angle), x + w/2, y + h/2);
         if (focused){
             g2d.setColor(Color.red);
             g2d.drawRect(this.x - 2,this.y - 2, this.w + 4,this.h + 4);
@@ -23,6 +23,7 @@ public class Rect extends Figure {
         g2d.fillRect(this.x, this.y, this.w, this.h);
         g2d.setColor(this.contorno);
         g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setTransform(old);
     }
 
 }
