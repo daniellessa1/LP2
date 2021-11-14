0c93dd82a0444941c2de440dfca9371521dc3a05
daniellessa1 <88343369+daniellessa1@users.noreply.github.com>
2021-11-14 10:52:57 -0300

    Update ListApp.java

--- Projeto/projeto 1-2/ListApp.java
+++ Projeto/projeto 1-2/ListApp.java
@@ -1,12 +1,14 @@
 import java.awt.*;
+import java.io.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 import java.awt.Color;
 import figures.*;
 import ivisible.IVisible;
 import botao.Botao;
+import org.jfree.svg.*;
+
 
 
 class ListApp {
@@ -23,64 +25,87 @@ class ListFrame extends JFrame {
     Random rand = new Random();
     Figure focus = null;
     Botao focus_button = null;
+    int angulo = 0;
     Color colors[] = {Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.orange, Color.pink, Color.red, Color.white, Color.yellow};
 
 
 
 
     ListFrame () {
-        
-        buts.add(new Botao(1,new Rect(0,0,0,0,Color.BLACK, Color.BLACK)));
-        buts.add(new Botao(2,new Ellipse(0,0,0,0,Color.BLACK, Color.BLACK)));
-        buts.add(new Botao(3,new Poly(0,0,0,0,Color.BLACK, Color.BLACK)));
-        buts.add(new Botao(4, new Line(0,0,0,0, Color.BLACK,Color.WHITE)));
 
-        try {
-            FileInputStream f = new FileInputStream("proj.bin");
-            ObjectInputStream o = new ObjectInputStream(f);
-            this.figs = (ArrayList<Figure>) o.readObject();
-            o.close();
-        } catch (Exception x) {
-            System.out.println("Erro!, não foi possivel abrir o arquivo" +x);
-        }
-        this.addWindowListener (
+        buts.add(new Botao(1,new Rect(0,0,0,0,angulo,Color.BLACK, Color.BLACK)));
+        buts.add(new Botao(2,new Ellipse(0,0,0,0,angulo,Color.BLACK, Color.BLACK)));
+        buts.add(new Botao(3,new Poly(0,0,0,0,angulo,Color.BLACK, Color.BLACK)));
+        buts.add(new Botao(4, new Triangle(0,0,0,0,angulo,Color.BLACK,Color.BLACK)));
+        buts.add(new Botao(5, new Line(0,0,0,0,angulo,Color.BLACK,Color.BLACK)));
+        
+
+       this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
-                    try {
-                        FileOutputStream f = new FileOutputStream("proj.bin");
-                        ObjectOutputStream o = new ObjectOutputStream(f);
-                        o.writeObject(figs);
-                        o.flush();
-                        o.close();
-                    } catch (Exception x) {
-                        System.out.println("Erro!, não foi possivel criar o arquivo" +x);
-                    }
                     System.exit(0);
                 }
             }
         );
 
-
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
+                   if (evt.getKeyCode() == KeyEvent.VK_F1){
+                        try {
+                            FileOutputStream f = new FileOutputStream("proj.bin");
+                            ObjectOutputStream o = new ObjectOutputStream(f);
+                            o.writeObject(figs);
+                            o.flush();
+                            o.close();
+                        } catch (Exception x) {
+                            System.out.println("Erro!, não foi possivel criar o arquivo" +x);
+                        }
+                    } else if(evt.getKeyCode() == KeyEvent.VK_F2){
+                        try {
+                            FileInputStream f = new FileInputStream("proj.bin");
+                            ObjectInputStream o = new ObjectInputStream(f);
+                            figs = (ArrayList<Figure>) o.readObject();
+                            o.close();
+                        } catch (Exception x) {
+                            System.out.println("Erro!, não foi possivel abrir o arquivo" +x);
+                        }
+                    } else if(evt.getKeyCode() == KeyEvent.VK_F3){
+                        try {
+                            SVGGraphics2D gSVG = new SVGGraphics2D(getWidth(), getHeight());
+                
+                            for (Figure fig: figs) {
+                                fig.paint(gSVG, false);
+                            }
+                
+                            File svgFile = new File("proj.svg");
+                            svgFile.delete();
+                            svgFile.createNewFile();
+                            SVGUtils.writeToSVG(svgFile, gSVG.getSVGElement());
+                        } catch (Exception error) {
+                            System.out.println("Não foi possível salvar o projeto em SVG.\nError: " + error);
+                        }
+                    }
                     Point ponto = getMousePosition();
                     if (ponto != null){
-                        int w = rand.nextInt(200);
-                        int h = rand.nextInt(200);
+                        int w = rand.nextInt(50);
+                        int h = rand.nextInt(50);
                         int x = ponto.x - w/2;
                         int y = ponto.y - h/2;
                         if (evt.getKeyChar() == 'r') {
-                            figs.add(new Rect(x,y, w,h,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Rect(x,y,w,h,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
 
                         } else if (evt.getKeyChar() == 'e') {
-                            figs.add(new Ellipse(x,y, w,h,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Ellipse(x,y,w,h,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
 
                         } else if (evt.getKeyChar() == 'l') {
-                            figs.add(new Line(x,y, w,h,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Line(x,y,w,h,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
 
                         } else if (evt.getKeyChar() == 'p') {
-                            figs.add(new Poly(x,y, w,h,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Poly(x,y,w,h,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                        
+                        } else if (evt.getKeyChar() == 't') {
+                            figs.add(new Triangle(x,y,w,h,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
 
                         } else if(evt.getKeyCode() == KeyEvent.VK_ESCAPE){
                             figs.clear();
@@ -99,13 +124,18 @@ class ListFrame extends JFrame {
                                 if(focus.w < 490 && focus.h < 490){
                                     focus.h = focus.h + 1;
                                     focus.w = focus.w + 1;
-                            }
+                                }
                             } else if(evt.getKeyChar() == '-'){
                                 if(focus.w > 5 && focus.h > 5 ){
                                     focus.h = focus.h - 1;
                                     focus.w = focus.w - 1;
-                            }
-                        
+                                }
+                            } else if (evt.getKeyChar() == 's') {
+                                focus.angle = focus.angle + 5;
+
+                            } else if (evt.getKeyChar() == 'a') {
+                                focus.angle = focus.angle - 5;
+
                             } else if(evt.getKeyCode() == KeyEvent.VK_UP){
                                 focus.y = focus.y - 10;
     
@@ -125,9 +155,32 @@ class ListFrame extends JFrame {
                         }
                         repaint();
                     }
+                
                 }
             }
         );
+
+        this.addMouseWheelListener (
+                new MouseWheelListener() {
+                    public void mouseWheelMoved(MouseWheelEvent evt) {
+                        int scroll = evt.getUnitsToScroll();
+
+                        if (focus != null) {
+                            if (focus.w + scroll > 4 && focus.h + scroll > 4) {
+                                if ((evt.getModifiersEx() & evt.SHIFT_DOWN_MASK) != 0)
+                                    focus.h += scroll;
+                                else if ((evt.getModifiersEx() & evt.ALT_DOWN_MASK) != 0)
+                                    focus.w += scroll;
+                                else {
+                                    focus.w += scroll;
+                                    focus.h += scroll;
+                                }
+                            }
+                            repaint();
+                        }
+                    }
+                }
+            );
 
         this.addMouseMotionListener(
         	new MouseMotionAdapter() {
@@ -143,15 +196,17 @@ class ListFrame extends JFrame {
         this.addMouseListener(
         	new MouseAdapter() {
         		public void mousePressed(MouseEvent evt) {
-                    if (focus_button!= null && !(evt.getX() >= 0 && evt.getX() <= 80 && evt.getY() >= 0 && evt.getY() <= 200)){
+        			if (focus_button!= null && !(evt.getX() >= 0 && evt.getX() <= 80 && evt.getY() >= 0 && evt.getY() <= 200)){
                         if(focus_button.idx==1)
-							figs.add(new Rect(evt.getX(),evt.getY(),25,25,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));      
+							figs.add(new Rect(evt.getX(),evt.getY(),50,50,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));      
 						else if(focus_button.idx==2)
-							figs.add(new Ellipse(evt.getX(),evt.getY(),25,25,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+							figs.add(new Ellipse(evt.getX(),evt.getY(),50,50,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
 						else if(focus_button.idx==3)
-                            figs.add(new Poly(evt.getX(),evt.getY(),25,25,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Poly(evt.getX(),evt.getY(),50,50,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
                         else if(focus_button.idx==4)
-                            figs.add(new Line(evt.getX(),evt.getY(),25,25,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                            figs.add(new Triangle(evt.getX(),evt.getY(),50,50,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
+                        else if(focus_button.idx==5)
+                            figs.add(new Line(evt.getX(),evt.getY(),50,50,angulo,colors[rand.nextInt(10)],colors[rand.nextInt(10)]));
                         repaint();
 
                     }
@@ -183,21 +238,18 @@ class ListFrame extends JFrame {
         		}
         	}
         );
-
         this.setTitle("Lista de Figuras");
         this.setSize(500, 500);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        super.paint(g);
+        super.paint(g2d);
         for (Figure fig: this.figs) {
-            fig.paint(g, fig == focus);
+            fig.paint(g2d, fig == focus);
         }
         for (Botao but: this.buts) {
-            but.paint(g, but == focus_button);
+            but.paint(g2d, but == focus_button);
         }
-        
     }
 }
-
