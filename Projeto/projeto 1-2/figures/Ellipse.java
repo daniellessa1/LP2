6d144174bc1319c82e838e401332e0b6b05364b0
daniellessa1 <88343369+daniellessa1@users.noreply.github.com>
2021-11-14 10:47:28 -0300

    Update Ellipse.java

--- Projeto/projeto 1-2/figures/Ellipse.java
+++ Projeto/projeto 1-2/figures/Ellipse.java
@@ -4,19 +4,17 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
 
 public class Ellipse extends Figure {
-    public Ellipse (int x, int y, int w, int h, Color contorno, Color fundo) {
-        super(x,y, w,h, contorno,fundo);
-    }
-
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+    public Ellipse (int x, int y, int w, int h,int angle, Color contorno, Color fundo) {
+        super(x,y,w,h,angle,contorno,fundo);
     }
 
     public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
+        AffineTransform old = g2d.getTransform();
+        g2d.rotate(Math.toRadians(angle), x + w/2, y + h/2);
         if (focused){
             g2d.setColor(Color.red);
             g2d.draw(new Ellipse2D.Double(this.x - 2, this.y - 2, this.w + 4, this.h + 4));
@@ -25,6 +23,7 @@ public class Ellipse extends Figure {
         g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h ));
         g2d.setColor(this.contorno);
         g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        g2d.setTransform(old);
     }
 }
 
