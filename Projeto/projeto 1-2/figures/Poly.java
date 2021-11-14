21d8b14d8d045e27646ef73b4ff4c9d10cddfb94
daniellessa1 <88343369+daniellessa1@users.noreply.github.com>
2021-11-14 10:49:02 -0300

    Update Poly.java

--- Projeto/projeto 1-2/figures/Poly.java
+++ Projeto/projeto 1-2/figures/Poly.java
@@ -2,25 +2,21 @@ package figures;
 
 import java.awt.*;
 import java.awt.Polygon;
+import java.awt.geom.AffineTransform;
 
 public class Poly extends Figure {
     int[] xPoint, yPoint;
     int npoints = 4;
-    public Poly (int x, int y, int w, int h, Color contorno, Color fundo) {
-        super(x,y, w,h, contorno,fundo);
-    }
-
-    public void print () {
-        for(int i = 0; i < npoints ; i++) {
-            System.out.format("Poligono de (%d) lados nos pontos (%d,%d).\n",
-            this.npoints, this.xPoint[i], this.yPoint[i]);
-          }  
+    public Poly (int x, int y, int w, int h,int angle, Color contorno, Color fundo) {
+        super(x,y,w,h,angle,contorno,fundo);
     }
 
     public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
         this.xPoint = new int[]{x + w/2 , x + w, x + w/2, x};
         this.yPoint = new int[]{y, y + h/2 , y + h, y + h/2 };
+        AffineTransform old = g2d.getTransform();
+        g2d.rotate(Math.toRadians(angle), x + w/2, y + h/2);
         if (focused){
             int[] xfocus = {x + w/2 , (x + w) + 2, x + w/2, x - 2};
             int[] yfocus = {y - 2 , y + h/2 , (y + h) + 2, y + h/2 };
@@ -31,5 +27,6 @@ public class Poly extends Figure {
         g2d.fill(new Polygon(this.xPoint, this.yPoint, this.npoints));
         g2d.setColor(this.contorno);
         g2d.draw(new Polygon(this.xPoint, this.yPoint, this.npoints));
+        g2d.setTransform(old);
     }
 }
